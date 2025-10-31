69554ae8c5f52dc16cac1c2bd67891ce66ac8eb6
Trey Fortmuller <tfortmuller@mac.com>
2025-10-30 23:16:54 -0700

    why does nix fmt not work in the slightest...

--- README.md
+++ README.md
@@ -1,3 +1,22 @@
-# Embedded Rust `hello-world`
+# Embedded rust `hello-world`
 
-TODO...
+This project works out of the embedded rust book found here: https://doc.rust-lang.org/beta/embedded-book/intro/index.html
+
+The toolchain to hack on this thing is in the provided dev shell:
+
+```
+nix develop
+
+cargo --version
+```
+
+The only thing not delivered to your system with the dev shell is udev rules you might want to enable in order to use OpenOCD with the discovery board without root privledges:
+
+```
+# STM32F3DISCOVERY rev A/B - ST-LINK/V2
+ATTRS{idVendor}=="0483", ATTRS{idProduct}=="3748", TAG+="uaccess"
+
+# STM32F3DISCOVERY rev C+ - ST-LINK/V2-1
+ATTRS{idVendor}=="0483", ATTRS{idProduct}=="374b", TAG+="uaccess"
+```
+
--- flake.nix
+++ flake.nix
@@ -5,31 +5,34 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
-  outputs = { self, nixpkgs }: let
-  system = "x86_64-linux";
-  pkgs = import nixpkgs { inherit system; };
-  in {
-    devShells.${system}.default = 
-    pkgs.mkShell {
-      buildInputs = with pkgs; [
-        rustc
-        cargo
-        cargo-binutils
-        # gdb-multiarch # TODO: this doesn't exist
-        probe-rs-tools
-        minicom
-      ];
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in
+    {
+      devShells.${system}.default =
+        pkgs.mkShell {
+          buildInputs = with pkgs; [
+            rustc
+            cargo
+            cargo-binutils
+            gcc-arm-embedded # Ships the arm-none-eabi-gdb binary
+            probe-rs-tools
+            minicom
+            nixpkgs-fmt
+          ];
 
-      # Optional: helpful environment variables for Rust dev
-      RUST_BACKTRACE = "1";
-      RUST_SRC_PATH = pkgs.rustPlatform.rustLibSrc;
+          # Optional: helpful environment variables for Rust dev
+          RUST_BACKTRACE = "1";
+          RUST_SRC_PATH = pkgs.rustPlatform.rustLibSrc;
 
-      shellHook = ''
-        echo "🦀 Evolved into a crab again... shit."
-        rustc --version
-      '';
+          shellHook = ''
+            echo "🦀 Evolved into a crab again... shit."
+            rustc --version
+          '';
+        };
+
+      formatter.${system} = pkgs.nixpkgs-fmt;
     };
-
-    formatter.${system} = pkgs.nixpkgs-fmt;
-  };
 }
